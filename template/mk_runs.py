a36f69ae9cbbabf407fbc9297051ed0b8f1bfb15
Peter Teuben <teuben@gmail.com>
2023-05-09 23:36:19 -0400

    better template

--- template/mk_runs.py
+++ template/mk_runs.py
@@ -1,36 +1,25 @@
 #! /usr/bin/env python
 #
-#   script generator for project="2021-S1-US-3"
-#
-#   lmtinfo.py grep US-3 Science Map | awk '{print $2}' | sort
-
 
 import os
 import sys
 
-# in prep of the new lmtoy module
-try:
-    lmtoy = os.environ['LMTOY']
-    sys.path.append(lmtoy)
-    from lmtoy import runs
-except:
-    print("No LMTOY with runs.py")
-    sys.exit(0)
+from lmtoy import runs
 
-project="2023-S1-US-18"
+project="2023-S1-US-00"
 
 #        obsnums per source (make it negative if not added to the final combination)
 on = {}
-on[''] = [ ]
+on['foo'] = [ ]
 
 
 #        common parameters per source on the first dryrun (run1a, run2a)
 pars1 = {}
-pars1['']   = ""
+pars1['foo']   = ""
 
 #        common parameters per source on subsequent runs (run1b, run2b)
 pars2 = {}
-pars2['']   = "srdp=1 admit=0"
+pars2['foo']   = "srdp=1 admit=0"
 
 if __name__ == '__main__':    
     runs.mk_runs(project, on, pars1, pars2)
